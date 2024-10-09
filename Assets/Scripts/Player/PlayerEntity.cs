c8013aea82228ca3a085578dbc5ed5ed4fbb17f1
Keimo922 <jarppe22@gmail.com>
2024-10-09 14:32:36 +0300

    Added Ravine prefab with kill logic

--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerEntity : Entity
 {
-    
+    public override void Kill()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
